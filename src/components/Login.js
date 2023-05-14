b4ecf73433a6d1f93714791674d6e7b73791d350
Sribastav <prajguru54@gmail.com>
2023-05-15 01:37:00 +0530

    Feature: Raw form without any styling

---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post("YOUR_LOGIN_API_URL", {
+                username,
+                password,
+            });
+            console.log(response.data); // Handle the response as needed
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Register.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const Register = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post("YOUR_REGISTER_API_URL", {
+                username,
+                password,
+            });
+            console.log(response.data); // Handle the response as needed
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Register</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
