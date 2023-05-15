9ff4ecc78090d9ca9e71c26ea8b93b2289fb8724
Sribastav <prajguru54@gmail.com>
2023-05-15 11:07:50 +0530

    Feature: Added register and login route

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,34 @@
-import axios from "axios";
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Form.css';
+import { serverPort } from '../constants';
+
+const loginUrl = `http://localhost:${serverPort}/auth/login`
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post("YOUR_LOGIN_API_URL", {
-                username,
-                password,
-            });
-            console.log(response.data); // Handle the response as needed
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(loginUrl, { username, password });
+      console.log(response.data); // Handle the response as needed
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="form-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,43 +1,35 @@
-import axios from "axios";
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Form.css';
+import { serverPort } from '../constants';
 
 const Register = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const registerUrl = `http://localhost:${serverPort}/auth/register`
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(registerUrl, { username, password });
+      console.log(response.data); // Handle the response as needed
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post("YOUR_REGISTER_API_URL", {
-                username,
-                password,
-            });
-            console.log(response.data); // Handle the response as needed
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    return (
-        <div>
-            <h2>Register</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Register</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="form-container">
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
