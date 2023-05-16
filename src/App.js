83413ef9bc4d51f16280338822eab1c6ddd6c657
Sribastav <prajguru54@gmail.com>
2023-05-16 11:24:20 +0530

    Feature: Added register and login route-Working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
+import { Route, Routes } from "react-router";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import { Link } from "react-router-dom";
 
 const App = () => {
     return (
-        <div>
-            <Login />
-            <Register />
-        </div>
+        <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/auth/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/auth/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+            <Routes>
+                <Route path="/auth/register" element={<Register />} />
+                <Route path="/" element={<Register />} />
+                <Route path="/auth/login" element={<Login />} />
+            </Routes>
+        </>
     );
 };
 
